d368fb768d6c63d02e6a7854ae3f37c7800d346e
s.suzuki <s.suzuki@creist.co.jp>
2021-08-10 20:39:08 +0900

    スプリントのテーブル定義

--- docker-mysql/mysql/DB/flask_sv.sql
+++ docker-mysql/mysql/DB/flask_sv.sql
@@ -106,6 +106,40 @@ LOCK TABLES `authority` WRITE;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
+--
+-- Table structure for table `sprint`
+--
+
+DROP TABLE IF EXISTS `sprint`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `sprint` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `project_id` int NOT NULL,
+  `sprint_name` varchar(64) NOT NULL,
+  `start_on` datetime NOT NULL,
+  `end_on` datetime NOT NULL,
+  `created_by` int DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_by` int DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `status` int NOT NULL,
+  `velocity` int DEFAULT NULL,
+  `total_plan_time` int DEFAULT NULL,
+  `total_working_time` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sprint`
+--
+
+LOCK TABLES `sprint` WRITE;
+/*!40000 ALTER TABLE `sprint` DISABLE KEYS */;
+/*!40000 ALTER TABLE `sprint` ENABLE KEYS */;
+UNLOCK TABLES;
+
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
